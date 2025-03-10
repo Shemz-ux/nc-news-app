9bd0a8fc28d222a3d68abf368d940b7f892cf277
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-10 14:18:46 +0000

    Made changes to previous test ticket and added test for the /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,26 +25,67 @@ describe("GET /api", () => {
   });
 });
 
+
 describe("GET /api/topics", () => {
-  test("200: Response with an array of topic objects, each of which contain a slug and description" , () => {
+  test("200: Responds with an array of topic objects, each of which contain a slug and description" , () => {
     return request(app)
     .get("/api/topics")
     .expect(200)
     .then(({body})=>{
       const {topics} = body
+      expect(topics).toHaveLength(3)
       topics.forEach((snack)=>{
         expect(typeof snack.slug).toBe('string')
         expect(typeof snack.description).toBe('string')
       })
     })
   })
+})
 
-  test("400: Responds with a 400 error when an invalid request is made to this endpoint", () => {
+describe("404 invalid endpoint /api/banana", () => {
+  test("404: Responds with a 404 error when an invalid request is made to this endpoint", () => {
     return request(app)
     .get("/api/bananas")
+    .expect(404)
+    .then(({body})=>{
+      expect(body.msg).toBe('Invalid request')
+    })
+  })
+})
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: Responds with an object containing the data by it article_id", () => {
+    return request(app)
+    .get("/api/articles/4")
+    .expect(200)
+    .then(({body})=>{
+      console.log(body)
+      const {author, title, article_id, created_at, votes, article_img_url } = body.topic
+      expect(typeof author).toBe('string')
+      expect(typeof title).toBe('string')
+      expect(typeof article_id).toBe('number')
+      expect(typeof created_at).toBe('string')
+      expect(typeof votes).toBe('number')
+      expect(typeof article_img_url).toBe('string')
+    })
+  })
+
+  test("400: Responds with an error when an invalid Id is used", () => {
+    return request(app)
+    .get("/api/articles/apples")
     .expect(400)
     .then(({body})=>{
       expect(body.msg).toBe('Invalid request')
+    })
+  })
+
+  test("404: Responds with an error when the article does not exist", () => {
+    return request(app)
+    .get("/api/articles/1000")
+    .expect(404)
+    .then(({body})=>{
+      console.log(body)
+      expect(body.msg).toBe('Not found')
     })
   })
 })
