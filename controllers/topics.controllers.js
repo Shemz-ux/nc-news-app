a57080a2ea88cd74e1c9673648aefa752fda79f8
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-10 14:22:07 +0000

    Error handlers for get article by id

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,18 @@
 // const express = require("express")
-const { fetchTopics } = require("../models/topics.model.js")
+const { fetchTopics, fetchTopicById } = require("../models/topics.model.js")
 
 
 exports.getTopics = (req, res) => {
     fetchTopics().then((topics) => {
         res.status(200).send({topics: topics})
+    })
+}
+
+exports.getTopicById = (req, res, next) => {
+    const {article_id} = req.params
+    fetchTopicById(article_id).then((topic) => {
+        res.status(200).send({topic: topic})
+    }).catch((err)=>{
+        next(err)
     })
 }
