13c3ff367e83eb179143bec89b62fffc9f60f0d8
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-10 16:12:30 +0000

    implementing the get articles function controller

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,11 +1,17 @@
-const { fetchArticleById } = require("../models/articles.model.js")
+const { fetchArticleById, fetchArticles } = require("../models/articles.model.js")
 
 
 exports.getArticleById = (req, res, next) => {
     const {article_id} = req.params
-    fetchArticleById(article_id).then((topic) => {
-        res.status(200).send({topic: topic})
+    fetchArticleById(article_id).then((article) => {
+        res.status(200).send({article: article})
     }).catch((err)=>{
         next(err)
+    })
+}
+
+exports.getArticles = (req, res, next) => {
+    fetchArticles().then((articles) => {
+        res.status(200).send({articles: articles})
     })
 }
