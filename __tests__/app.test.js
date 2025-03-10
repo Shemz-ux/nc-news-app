0e436a543a3dc766c784e471b63009911b548d86
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-10 10:11:16 +0000

    Api file setup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,14 @@
 const endpointsJson = require("../endpoints.json");
+const request = require("supertest")
+const app = require("../app.js")
+const db = require("../db/connection.js")
+const seed = require("../db/seeds/seed.js")
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
 
 describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
