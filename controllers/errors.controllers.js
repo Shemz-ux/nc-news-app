31f97d7da98ba09682ca5b1b7920568fc61be271
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-10 14:21:09 +0000

    get topic by id function made

--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -1,3 +1,18 @@
+
+exports.psqlErrorHandler = (err, req, res, next) => {
+    if (err.code === '22P02'){
+        res.status(400).send({msg: 'Invalid request'})
+    }
+    next(err)
+}
+
+exports.customErrorHandler = (err, req, res, next) => {
+    if (err.status){
+        res.status(err.status).send({msg: err.msg})
+    }
+    next(err)
+}
+
 exports.serverErrorHandler = (err, req, res, next) => {
     res.status(500).send({msg: 'Internal server error'})
 }
