2bdf1f354b564dd1e7917f31d601b569a74c555d
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-10 11:51:06 +0000

    Updated the endpoint to contain /api/snacks with other error handler functions

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const endpoints = require("./endpoints.json")
+const endpoints = require('./endpoints.json')
+const { getTopics } = require('./controllers/topics.controllers')
+const { serverErrorHandler} = require('./controllers/errors.controllers')
 
 app.get("/api", (req, res) => {
     res.status(200).send({endpoints})
 })
 
+app.get("/api/topics", getTopics)
 
+app.all("/*", (req, res) => {
+    res.status(400).send({msg: 'Invalid request'})
+})
+
+app.use(serverErrorHandler)
 
 module.exports = app
