c722b2cc3cc14d56d1398148ad1193ced0344195
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-11 15:23:22 +0000

    Updated to include Patch endpoint

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const endpoints = require('./endpoints.json')
 const { getTopics } = require('./controllers/topics.controllers')
-const { getArticleById, getArticles } = require('./controllers/articles.controllers')
+const { getArticleById, getArticles, patchArticleById } = require('./controllers/articles.controllers')
 const { getCommentsByArticleId, postCommentByArticleId } = require('./controllers/comments.controllers')
 const { serverErrorHandler, psqlErrorHandler, customErrorHandler } = require('./controllers/errors.controllers')
 
@@ -21,6 +21,8 @@ app.get("/api/articles", getArticles)
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 
 app.post("/api/articles/:article_id/comments", postCommentByArticleId)
+
+app.patch("/api/articles/:article_id", patchArticleById)
 
 app.all("/*", (req, res) => {
     res.status(404).send({msg: 'Invalid request'})
