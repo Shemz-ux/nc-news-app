8b6329dfb6a5fcb9fc66fce4656df94c3f0a3404
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-11 10:17:43 +0000

    created functions to interact with the db to access comment data

--- models/comments.models.js
+++ models/comments.models.js
@@ -1,10 +1,11 @@
 const db = require('../db/connection')
 
 exports.fetchCommentsByArticleId = (id) => {
-    return db.query(`select * from comments where article_id = $1 order by created_at desc`, [id])
+    return db.query(`SELECT * FROM comments 
+        JOIN articles ON comments.article_id = articles.article_id
+        WHERE articles.article_id = $1
+        ORDER BY articles.created_at DESC`, [id])
     .then(({rows})=>{
         return rows
-    }).catch((err)=>{
-        next(err)
     })
 }
