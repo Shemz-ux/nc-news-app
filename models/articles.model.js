771dd00dbe8754b720daf5aa61d7c40f14f9a1ab
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-11 15:25:46 +0000

    Making queries to update article table with vote increments

--- models/articles.model.js
+++ models/articles.model.js
@@ -20,4 +20,14 @@ exports.fetchArticles = () => {
     })
 }
 
+exports.updateArticle = (id, inc_votes) => {
+    return db.query(`UPDATE articles
+    SET votes = votes + $1 WHERE article_id = $2 RETURNING *`, [inc_votes, id])
+    .then(({rows})=>{
+        if (rows.length === 0){
+            return Promise.reject({status: 404, msg: 'Not found'})
+        }
+        return rows[0]
+    })
+}
 
