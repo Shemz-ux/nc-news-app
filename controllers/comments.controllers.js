ba1156a998cdbc66d1ba52fd09a06cf36d1aa5a5
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-11 10:16:13 +0000

    Created controllers to serve up responses for comments by its article_id

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,8 +1,18 @@
 const { fetchCommentsByArticleId } = require("../models/comments.models")
+const { checkArticleExists } = require("../models/filter.articles")
 
-exports.getCommentsByArticleId = (req, res) => {
+exports.getCommentsByArticleId = (req, res, next) => {
     const {article_id} = req.params
-    fetchCommentsByArticleId(article_id).then((comments)=>{
-        res.status(200).send({comments: comments})
+    const promises = [fetchCommentsByArticleId(article_id)]
+    
+    if (article_id){
+        promises.push(checkArticleExists(article_id))
+    }
+
+    Promise.all(promises).then((resolvedPromises)=>{
+        res.status(200).send({comments: resolvedPromises[0]})
+    }).catch((err)=>{
+        next(err)
     })
+
 }
