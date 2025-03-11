740963e4f60659705048b1ee7565cb036ceb7c33
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-11 13:39:03 +0000

    Updated to include new post request

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const endpoints = require('./endpoints.json')
 const { getTopics } = require('./controllers/topics.controllers')
 const { getArticleById, getArticles } = require('./controllers/articles.controllers')
-const { getCommentsByArticleId } = require('./controllers/comments.controllers')
+const { getCommentsByArticleId, postCommentByArticleId } = require('./controllers/comments.controllers')
 const { serverErrorHandler, psqlErrorHandler, customErrorHandler } = require('./controllers/errors.controllers')
+
+app.use(express.json())
 
 app.get("/api", (req, res) => {
     res.status(200).send({endpoints})
@@ -17,6 +19,8 @@ app.get("/api/articles/:article_id", getArticleById)
 app.get("/api/articles", getArticles)
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
+
+app.post("/api/articles/:article_id/comments", postCommentByArticleId)
 
 app.all("/*", (req, res) => {
     res.status(404).send({msg: 'Invalid request'})
