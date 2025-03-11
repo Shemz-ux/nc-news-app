b85b62aa91dbf9123cefc6894ac1c8ebee5ea94c
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-11 13:39:59 +0000

    updated to include a post comment by article id function

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,4 +1,5 @@
-const { fetchCommentsByArticleId } = require("../models/comments.models")
+const { lookUpByUsername } = require("../db/seeds/utils")
+const { fetchCommentsByArticleId, insertCommentByArticleId } = require("../models/comments.models")
 const { checkArticleExists } = require("../models/filter.articles")
 
 exports.getCommentsByArticleId = (req, res, next) => {
@@ -15,4 +16,23 @@ exports.getCommentsByArticleId = (req, res, next) => {
         next(err)
     })
 
+}
+
+exports.postCommentByArticleId = (req, res, next) => {
+    const {article_id} = req.params
+    const comment = req.body
+    checkArticleExists(article_id).then(()=>{
+        lookUpByUsername(article_id).then((rows)=>{
+            const {author} = rows[0]
+            comment.article_id = article_id
+            comment.author = author
+            delete comment.username
+            insertCommentByArticleId(comment)
+            .then((newComment)=>{
+                res.status(201).send({newComment: newComment} )
+            })
+        })
+    }).catch((err)=>{
+        next(err)
+    })
 }
