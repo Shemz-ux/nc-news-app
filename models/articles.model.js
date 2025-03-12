c437c4f3a99075b6dbfe0544f9b8d620fc6df0ac
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-12 14:35:34 +0000

    Updated to handle topic query

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection")
+const format = require("")
 
 exports.fetchArticleById = (id) => {
     return db.query(`select * from articles where article_id = $1`, [id])
@@ -14,15 +15,19 @@ exports.fetchArticles = (query) => {
     let queryStr = `
         SELECT articles.*, COUNT(comments.comment_id)::INT AS comment_count 
         FROM articles 
-        LEFT JOIN comments ON articles.article_id = comments.article_id 
-        GROUP BY articles.article_id
-    `;
+        LEFT JOIN comments ON articles.article_id = comments.article_id`;
 
-    const validSortColumns = ["created_at", "article_id", "votes"]
-    const validOrderOptions = ["asc", "desc"]
+    const validSortColumns = ["created_at", "article_id", "votes"];
+    const validOrderOptions = ["asc", "desc"];
+    const queryValues = [];
 
-    let sortBy = "created_at"
-    let order = "desc"
+    let sortBy = "created_at";
+    let order = "desc";
+
+    if (query.topic) {
+        queryStr += ` WHERE articles.topic = $1`;
+        queryValues.push(query.topic);
+    }
 
     if (query.sort_by) {
         if (!validSortColumns.includes(query.sort_by)) {
@@ -38,11 +43,14 @@ exports.fetchArticles = (query) => {
         order = query.order.toLowerCase();
     }
 
-    queryStr += ` ORDER BY articles.${sortBy} ${order}`;
+    queryStr += ` GROUP BY articles.article_id ORDER BY articles.${sortBy} ${order}`;
 
-    return db.query(queryStr).then(({ rows }) => {
-        return rows
-    })
+    return db.query(queryStr, queryValues).then(({ rows }) => {
+        if (rows.length === 0){
+            return Promise.reject({status: 400, msg: 'Invalid query'})
+        }
+        return rows;
+    });
 }
 
 exports.updateArticle = (id, inc_votes) => {
