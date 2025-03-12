871a0f7f50098cccf87516ef473aa705e78166e1
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-12 15:45:40 +0000

    Connection setupt

--- db/connection.js
+++ db/connection.js
@@ -12,13 +12,12 @@ if (ENV === "production") {
     config.connectionString = process.env.DATABASE_URL;
     config.max = 2;
   }
-  module.exports = new Pool(config);
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set");
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
 
-module.exports = db;
+module.exports = db, new Pool(config);
