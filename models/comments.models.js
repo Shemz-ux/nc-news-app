a5b24b3f573ce88680fcc9f6db43bb7ec6b93992
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-12 09:09:13 +0000

    Updated to include query to delete coment by id

--- models/comments.models.js
+++ models/comments.models.js
@@ -21,6 +21,9 @@ VALUES ($1, $2, $3) RETURNING *`, [article_id, body, author]).then(({rows})=>{
 exports.removeCommentById = (id) => {
     return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *`, [id])
     .then(({rows})=>{
+        if (rows.length === 0){
+            return Promise.reject({status: 404, msg: 'Not found'})
+        }
         return rows
     })
 }
