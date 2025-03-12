5c12051708ab1db25f484a23e7034e68cec2a714
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-12 15:40:12 +0000

    Setting up hosting in connection files

--- db/connection.js
+++ db/connection.js
@@ -6,6 +6,14 @@ require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
 const db = new Pool();
 
+const config = {}
+
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+  }
+  module.exports = new Pool(config);
+
 if (!process.env.PGDATABASE) {
     throw new Error("No PGDATABASE configured")
 } else { 
