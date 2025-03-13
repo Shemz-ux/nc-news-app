7191b62a3e53f4e64951197c2fe6d6d17ffaf8d3
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-13 10:12:51 +0000

    Updated scripts

--- db/connection.js
+++ db/connection.js
@@ -1,23 +1,24 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
-
-const db = new Pool();
-
-const config = {}
-
-if (ENV === "production") {
-    config.connectionString = process.env.DATABASE_URL;
-    config.max = 2;
-  }
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("PGDATABASE or DATABASE_URL not set");
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
+const config = {};
 
-module.exports = db
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+const db = new Pool(config);
+
+if (ENV !== "test") {
+  console.log(`Connected to ${process.env.PGDATABASE || "production database"}`);
+}
+
+module.exports = db;
